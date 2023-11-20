82e8ae8e9f05710cb22715228a2faf1e76a0499e
vladumbrava <vladdumbrava5@gmail.com>
2023-11-20 20:43:33 +0200

    Almost done

--- lab_assignment_3/main.py
+++ lab_assignment_3/main.py
@@ -5,61 +5,184 @@ sa nu uit pt minimum si maximum distance sa fac sa se afiseze numarul si doar 2 
 '''
 
 def main():
-    '''
-        Initializare cateva puncte
-    '''
-    new_point = MyPoint(-1, 4, "blue")
-    new_point1 = MyPoint(-6, 6, "red")
-    new_point3 = MyPoint(-10, 5, "red")
-    new_point4 = MyPoint(5, 4, "red")
-    new_point5 = MyPoint(1,2,"red")
-    new_point6 = MyPoint(1,1,"red")
 
-    new_list = [new_point, new_point1, new_point3, new_point4, new_point5, new_point6]
-
-    '''
-            Initializare repository
-    '''
     repository = PointRepository()
-    '''
-                Setare lista puncte si afisare
-    '''
-    repository.set_list_of_points(new_list)
+    point1 = MyPoint(-1,-5,"red")
+    point2 = MyPoint(-10,2,"red")
+    point3 = MyPoint(4,6,"blue")
+    point4 = MyPoint(1,-2,"blue")
+    point5 = MyPoint(2,4,"blue")
+    point6 = MyPoint(-6,14,"yellow")
+    point7 = MyPoint(-20,6,"green")
+    pointlist = [point1,point2,point3,point4,point5,point6,point7]
 
-    '''
-                Am creat punct nou, l-am adaugat la 
-                repository si afisare
-    '''
-    new_point_to_add = MyPoint(-2, 25, "black")
-    repository.add_new_point(new_point_to_add)
+    repository.set_list_of_points(pointlist)
 
-    list_of_points_of_given_color = repository.get_points_given_color("red")
-    print("List of points of given color: ")
-    for point in list_of_points_of_given_color:
-        print(point)
+    while True:
+        print("\nMenu")
+        print("1. Add a point")
+        print("2. Get all points")
+        print("3. Get a point at a given index")
+        print("4. Get all points of a given color")
+        print("5. Get all points inside a given square")
+        print("6. Get the minimum distance between two points")
+        print("7. Update a point at a given index")
+        print("8. Delete a point by index")
+        print("9. Delete all points that are inside a given square")
+        print("10. Plot all points in a chart")
+        print("11. Extra functions")
+        print("12. Exit")
 
-    list_of_points_inside_square = repository.get_points_inside_square(MyPoint(4, 5, None), 3)
-    print("List of points in the square: ")
-    for point in list_of_points_inside_square:
-        print(point)
+        choice = int(input("Enter your choice: "))
 
-    print(repository.get_min_dist_between_points())
+        if choice == 1:
+            while True:
+                print("Choose the properties of your point:")
+                try:
+                    coord_x = float(input("Enter the X: "))
+                    coord_y = float(input("Enter the Y: "))
+                except ValueError:
+                    print("Invalid input. Please enter proper properties.")
+                    break
+                color = input("Enter the color('red', 'green', 'blue', 'yellow', 'magenta'): ")
+                if color != "red" and color != "green" and color != "blue" and color != "yellow" and color != "magenta":
+                    print("Invalid input. Please enter proper properties.")
+                    break
+                else:
+                    repository.add_new_point(MyPoint(coord_x, coord_y, color))
+                    print(repository)
+                    break
 
-    print(repository)
-    repository.update_point_given_index(1,2,3,"blue")
-    print("Updated list after updating point at index 1: ")
-    print(repository)
+        if choice == 2:
+            print(f"The points in the repository are:")
+            for point in repository.get_list_of_points():
+                print(point)
 
-    repository.delete_point_by_index(1)
-    print(repository)
-    repository.delete_points_inside_square(MyPoint(4,5,None),3)
-    print(repository)
-    repository.extra_delete_points_within_distance_from_point(MyPoint(6,8,None),8)
-    print(repository)
+        if choice == 3:
+            try:
+                index = int(input("Please enter the index:"))
+                if 0 <= index <= len(repository.get_list_of_points())-1:
+                    print(f"The point at index {index} is: {repository.get_point_given_index(index)}")
+                else:
+                    print("Invalid input. Please enter an appropriate value for index.")
+            except ValueError:
+                print("Invalid input. Please enter an integer value.")
 
-    # print(repository.extra_maximum_distance_2_points())
-    repository.extra_shift_on_x(100)
-    print(repository)
+        if choice == 4:
+            color = input("Please enter a color: ")
+            points_of_given_color = repository.get_points_given_color(color)
+            if color != "red" and color != "green" and color != "blue" and color != "yellow" and color != "magenta":
+                print("Invalid input. Please enter a proper color.")
+            else:
+                if len(points_of_given_color) == 0:
+                    print(f"There are no points of color {color}.")
+                else:
+                    print(f"The points of color {color} are the following:")
+                    for point in points_of_given_color:
+                        print(point)
+
+        if choice == 5:
+            print("Enter the coordinates of the upper left corner of the square: ")
+            try:
+                corner_x = float(input("Enter the X: "))
+                corner_y = float(input("Enter the Y: "))
+            except ValueError:
+                print("Invalid input. Please enter proper coordinates.")
+                break
+            try:
+                length = float(input("Enter the length of the square: "))
+            except ValueError:
+                print("Invalid input. Please enter a proper length.")
+                break
+            print(repository.get_points_inside_square(MyPoint(corner_x,corner_y,None),length))
+
+        if choice == 6:
+            print(f"The minimum distance between 2 points from our list is: {repository.get_min_dist_between_points()}")
+
+        if choice == 7:
+            try:
+                index = int(input("Enter the index of the point: "))
+                if 0 <= index < len(repository.get_list_of_points()):
+                    try:
+                        updated_x = float(input("Enter the updated X: "))
+                        updated_y = float(input("Enter the updated Y: "))
+                    except ValueError:
+                        print("Invalid input. Please enter proper coordinates.")
+                        break
+                    color = input("Enter the updated color: ")
+                    if color != "red" and color != "green" and color != "blue" and color != "yellow" and color != "magenta":
+                        print("Invalid input. Please enter proper properties.")
+                        break
+                else:
+                    print("Invalid input. Enter a proper index.")
+            except ValueError:
+                print("Invalid input. Enter a proper index.")
+                break
+            repository.update_point_given_index(index, updated_x, updated_y, color)
+            print(f"The point at index {index} is now: ")
+            print(repository.update_point_given_index(index, updated_x, updated_y, color))
+
+        if choice == 8:
+            try:
+                index = int(input("Enter the index: "))
+                if 0 <= index < len(repository.get_list_of_points()):
+                    repository.delete_point_by_index(index)
+                else:
+                    print("Invalid input. Please enter a proper index.")
+            except ValueError:
+                print("Invalid input. The index should be an integer.")
+
+        if choice == 9:
+            print("Enter the coordinates of the upper left corner of the square: ")
+            try:
+                corner_x = float(input("Enter the X: "))
+                corner_y = float(input("Enter the Y: "))
+            except ValueError:
+                print("Invalid input. Please enter proper coordinates.")
+
+            try:
+                length = float(input("Enter the length of the square: "))
+            except ValueError:
+                print("Invalid input. Please enter a proper length.")
+
+            repository.delete_points_inside_square(MyPoint(corner_x,corner_y,None),length)
+
+        #if choice == 10:
+
+        if choice == 11:
+
+            print("1. Delete all points within a certain distance from a given point")
+            print("2. Get the maximum distance between 2 points")
+            print("3. Shift all points on the X axis")
+
+            extra_choice = int(input("Enter your choice: "))
+
+            if extra_choice == 1:
+                print("Enter the coordinates of the point: ")
+                try:
+                    x = float(input("Enter the X: "))
+                    y = float(input("Enter the Y: "))
+                except ValueError:
+                    print("Invalid input. Please enter proper coordinates.")
+                try:
+                    distance = float(input("Enter the distance: "))
+                except ValueError:
+                    print("Invalid input. Please enter a proper distance.")
+                repository.extra_delete_points_within_distance_from_point(MyPoint(x, y,None),distance)
+
+            if extra_choice == 2:
+                print(f"The maximum distance between 2 points from our list is: {repository.extra_maximum_distance_2_points()}")
+
+            if extra_choice == 3:
+                try:
+                    x_value = float(input("Enter the amount you want to shift the points by: "))
+                except ValueError:
+                    print("Invalid input. You should enter a proper value.")
+                repository.extra_shift_on_x(x_value)
+
+
+
+
 
 
 if __name__ == "__main__":
--- lab_assignment_3/pointrepo.py
+++ lab_assignment_3/pointrepo.py
@@ -1,7 +1,6 @@
 from math import sqrt
 import matplotlib.pyplot as plt
 
-
 class PointRepository:
     def __init__(self):
         self.points_list = []
@@ -27,58 +26,14 @@ class PointRepository:
 
     def get_points_inside_square(self, corner, length):
         list = []
+        result = "The points inside the given square are: "
         for point in self.points_list:
             if corner.coord_x <= point.coord_x <= corner.coord_x + length \
                     and corner.coord_y - length <= point.coord_y <= corner.coord_y:
                 list.append(point)
-        return list
-
-    def update_point_given_index(self, index, x, y, color):
-        self.points_list[index].coord_x = x
-        self.points_list[index].coord_y = y
-        self.points_list[index].color = color
-        return self.points_list[index]
-
-    def delete_point_by_index(self, index):
-        self.points_list.pop(index)
-
-    def delete_points_inside_square(self, corner, length):
-        points_to_remove = [point for point in self.points_list
-                            if corner.coord_x <= point.coord_x <= corner.coord_x + length
-                            and corner.coord_y - length <= point.coord_y <= corner.coord_y]
-
-        for point in points_to_remove:
-            self.points_list.remove(point)
-
-    def extra_delete_points_within_distance_from_point(self, given_point, distance):
-        points_to_remove = [point for point in self.points_list
-                            if sqrt(pow(given_point.coord_x - point.coord_x, 2) +
-                                    pow(given_point.coord_y - point.coord_y, 2)) <= distance]
-        for point in points_to_remove:
-            self.points_list.remove(point)
-
-    def extra_maximum_distance_2_points(self):
-        max_distance = sqrt(pow(self.points_list[1].coord_x - self.points_list[0].coord_x, 2) +
-                            pow(self.points_list[1].coord_y - self.points_list[0].coord_y, 2))
-        for point1 in self.points_list:
-            for point2 in self.points_list:
-                if point1 != point2:
-                    dist = sqrt(pow(point2.coord_x - point1.coord_x, 2) + pow(point2.coord_y - point1.coord_y, 2))
-                    if dist > max_distance:
-                        max_distance = dist
-        return max_distance
-
-    def extra_shift_on_x(self, x_value):
-        for point in self.points_list:
-            point.coord_x += x_value
-
-
-
-
-
-
-
-
+        for point in list:
+            result += f"\n{point}"
+        return result
 
     def get_min_dist_between_points(self):
         min_distance = sqrt(pow(self.points_list[1].coord_x - self.points_list[0].coord_x, 2) +
@@ -89,7 +44,28 @@ class PointRepository:
                     dist = sqrt(pow(point2.coord_x - point1.coord_x, 2) + pow(point2.coord_y - point1.coord_y, 2))
                     if dist < min_distance:
                         min_distance = dist
-        return min_distance
+        return round(min_distance,2)
+
+    def update_point_given_index(self, index, x, y, color):
+        if 0 <= index < len(self.points_list):
+            self.points_list[index].coord_x = x
+            self.points_list[index].coord_y = y
+            self.points_list[index].color = color
+            return self.points_list[index]
+        else:
+            return None
+
+    def delete_point_by_index(self, index):
+        if 0 <= index < len(self.points_list):
+            self.points_list.pop(index)
+
+    def delete_points_inside_square(self, corner, length):
+        points_to_remove = [point for point in self.points_list
+                            if corner.coord_x <= point.coord_x <= corner.coord_x + length
+                            and corner.coord_y - length <= point.coord_y <= corner.coord_y]
+
+        for point in points_to_remove:
+            self.points_list.remove(point)
 
     def plot_all_points(self):
         colors = {'red': 'r', 'blue': 'b', 'black': 'k', 'green': 'g', 'yellow': 'y', 'purple': 'm', 'cyan': 'c',
@@ -107,8 +83,31 @@ class PointRepository:
         plt.title('Plot of Points')
         plt.legend()
         plt.show()
+
+    def extra_delete_points_within_distance_from_point(self, given_point, distance):
+        points_to_remove = [point for point in self.points_list
+                            if sqrt(pow(given_point.coord_x - point.coord_x, 2) +
+                                    pow(given_point.coord_y - point.coord_y, 2)) <= distance]
+        for point in points_to_remove:
+            self.points_list.remove(point)
+
+    def extra_maximum_distance_2_points(self):
+        max_distance = sqrt(pow(self.points_list[1].coord_x - self.points_list[0].coord_x, 2) +
+                            pow(self.points_list[1].coord_y - self.points_list[0].coord_y, 2))
+        for point1 in self.points_list:
+            for point2 in self.points_list:
+                if point1 != point2:
+                    dist = sqrt(pow(point2.coord_x - point1.coord_x, 2) + pow(point2.coord_y - point1.coord_y, 2))
+                    if dist > max_distance:
+                        max_distance = dist
+        return round(max_distance,2)
+
+    def extra_shift_on_x(self, x_value):
+        for point in self.points_list:
+            point.coord_x += x_value
+
     def __str__(self):
         result = "The points list is: "
-        for i in range(len(self.points_list)):
-            result += f"\n{self.points_list[i]}"
+        for point in self.points_list:
+            result += f"\n{point}"
         return result
